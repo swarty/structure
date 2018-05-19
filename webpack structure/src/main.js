f35386b4be9e95916c4bfde27f7787a383aba1cf
swarty_d <swarty16@gmail.com>
2018-05-19 22:58:45 +0300

    update structure

---
+++ webpack structure/src/main.js
@@ -0,0 +1,13 @@
+// import vue part
+import Vue from 'vue'
+import App from './App.vue'
+
+
+// import scss part
+import styles from './assets/styles/main.sass'
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})

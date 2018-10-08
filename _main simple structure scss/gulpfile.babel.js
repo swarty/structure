ac3dcfcc727e198bc05b91e8bc39f35252a55137
d.ogorodnykov <swarty16@gmail.com>
2018-10-08 15:21:46 +0300

    update structure

--- _main simple structure scss/gulpfile.babel.js
+++ _main simple structure scss/gulpfile.babel.js
@@ -1,29 +1,27 @@
-//initialize all of our variables
-var include, connect, app, wait, babel, babelPreset, minify, pug, base, concat, directory, gulp, gutil, hostname, path, refresh, sass, imagemin, minifyCSS, del, browserSync, autoprefixer, gulpSequence, shell, sourceMaps, plumber;
-
-var autoPrefixBrowserList = ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'];
+// versions browser
+var autoPrefixBrowserList = ['last 2 version', 'safari 5', 'ie 11', 'opera 12.1', 'ios 6', 'android 4'];
 
 //load all of our dependencies
 //add more here if you want to include more libraries
-gulp = require('gulp');
-pug = require('gulp-pug');
-gutil = require('gulp-util');
-concat = require('gulp-concat');
-babel = require('gulp-babel');
-babelPreset = require('babel-preset-env');
-minify = require('gulp-babel-minify');
-sass = require('gulp-sass');
-sourceMaps = require('gulp-sourcemaps');
-imagemin = require('gulp-imagemin');
-minifyCSS = require('gulp-minify-css');
-browserSync = require('browser-sync');
-autoprefixer = require('gulp-autoprefixer');
-gulpSequence = require('gulp-sequence').use(gulp);
-shell = require('gulp-shell');
-plumber = require('gulp-plumber');
-wait = require('gulp-wait');
-fileInclude = require('gulp-file-include');
-connect = require('gulp-connect');
+import gulp from 'gulp';
+import pug from 'gulp-pug';
+import gutil from 'gulp-util';
+import concat from 'gulp-concat';
+import babel from 'gulp-babel';
+import babelPreset from 'babel-preset-env';
+import minify from 'gulp-babel-minify';
+import sass from 'gulp-sass';
+import sourceMaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+import cleanCSS from 'gulp-clean-css';
+import browserSync from 'browser-sync';
+import autoprefixer from 'gulp-autoprefixer';
+import gulpSequence from 'gulp-sequence';
+import shell from 'gulp-shell';
+import plumber from 'gulp-plumber';
+import wait from 'gulp-wait';
+import fileInclude from 'gulp-file-include';
+import connect from 'gulp-connect';
 
 gulp.task('browserSync', function() {
     browserSync({
@@ -55,7 +53,9 @@ gulp.task('babel', function () {
 gulp.task('css', function() {
     return gulp.src('dist/styles/*.css')
     .pipe(plumber())
-    .pipe(minifyCSS())
+    .pipe(cleanCSS({
+			inline: ['all'] // enables all inlining, same as ['local', 'remote']
+		}))
     .pipe(gulp.dest('dist/styles/'));
 })
 

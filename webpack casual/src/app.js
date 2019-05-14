af7504c2d4b10ce4a9da5eca3fbde40cd6df2dde
d.ogorodnykov <d.ogorodnikov@lestro.com>
2019-05-14 16:07:15 +0300

    update styles

---
+++ webpack casual/src/app.js
@@ -0,0 +1,16 @@
+/* src/app.js */
+
+// Styles
+import 'styles/app.sass';
+
+import {app} from 'scripts/main';
+
+app();
+
+// import demo from demo;
+
+// $(document).ready(() => {
+//   console.log('Ready!');
+
+//   // require('scripts/demo');
+// });
---
+++ webpack casual/src/assets/scripts/main.js
@@ -0,0 +1,3 @@
+export function app() {
+	console.log('init app!!')
+}

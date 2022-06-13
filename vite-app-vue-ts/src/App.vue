d66fc0823d0db298c290c3f7e78dbd8fd7b82510
Dmytro <ohorodnykov@convidera.com>
2022-06-13 12:31:23 +0300

    update structure

--- vite-app-vue-ts/src/App.vue
+++ vite-app-vue-ts/src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <img alt="Vue logo" src="./assets/image/logo.png" />
     <HelloWorld msg="Hello Vue 3 + Vite" />
+    <CHeader />
   </div>
 </template>
 
@@ -9,6 +10,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue';
+import CHeader from './components/header/header.vue';
 </script>
 
 <style lang="scss">
---
+++ vite-app-vue-ts/src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hello header</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'cc-header',
+});
+</script>
+
+<script lang="ts" setup>
+// const data: string = '';
+</script>

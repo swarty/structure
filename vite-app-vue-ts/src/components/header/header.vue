81f52e4b752e954af72112067d2eddff632b5163
Dmytro <ohorodnykov@convidera.com>
2022-06-15 11:27:45 +0300

    updates for structure

--- vite-app-vue-ts/src/components/header/header.vue
+++ vite-app-vue-ts/src/components/header/header.vue
@@ -1,15 +1,9 @@
 <template>
-  <div>hello header</div>
+  <div class="header">
+    data
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'cc-header',
-});
-</script>
-
-<script lang="ts" setup>
-// const data: string = '';
-</script>
+<style lang="scss" scoped>
+  @import './header.scss';
+</style>
